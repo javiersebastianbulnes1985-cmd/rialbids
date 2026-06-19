8db18be2abb0ecf7603989790a2f05e6bb69752c
javiersebastianbulnes1985-cmd <javiersebastianbulnes1985@gmail.com>
2026-06-19 02:39:59 +0000

    fix: precio minimo lote €20

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -54,7 +54,7 @@ class VendorController extends \Illuminate\Routing\Controller
 
         $request->validate([
             'title'       => 'required|string|max:255',
-            'base_price'  => 'required|numeric|min:1',
+            'base_price'  => 'required|numeric|min:20',
             'duracion'    => 'required|integer|in:7,14,21,30',
             'description' => 'required|string|min:80',
             'lot_category'=> 'required|string',
@@ -162,7 +162,7 @@ class VendorController extends \Illuminate\Routing\Controller
         $request->validate([
             'title'       => 'required|string|max:255',
             'description' => 'required|string|min:80',
-            'base_price'  => 'required|numeric|min:1',
+            'base_price'  => 'required|numeric|min:20',
             'condition'   => 'required|string',
         ]);
 
